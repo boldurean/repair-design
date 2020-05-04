a08c7a4a0a24ba330e9bddf535ad4f8cd59f5ded
YesNoOkBye <yesnookbye@gmail.com>
2020-05-04 16:03:39 +0100

    Little changes to gulpfile.js using new sintax

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,32 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const sass = require("gulp-sass");
 
-gulp.task('minify-css', () => {
+function minify() {
   return gulp.src('css/*.css')
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('css'));
-});
+}
 
+function style() {
+  return gulp.src('./sass/**/*.sass')
+      .pipe(sass())
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream())
+}
 
-// Static server
-gulp.task('browser-sync', function() {
+function watch() {
   browserSync.init({
     server: {
       baseDir: "./"
     }
-  });
-  gulp.watch("./css/*.css");
+  })
+  gulp.watch('./sass/**/*.sass', style);
   gulp.watch("./*.html").on('change', browserSync.reload);
-});
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.minify = minify;
