3c21933429703832f36a3f7201dfa6fcb1dbf9e9
vsbdn <v.boldurean@gmail.com>
2020-05-06 11:46:02 +0100

    feat: adding gulp sass settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,34 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 
 function minify() {
-  return gulp.src('src/css/*.css')
+  return src('src/css/*.css')
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('dist/css'));
 }
 
 function style() {
-  return gulp.src('./src/sass/**/*.sass')
+  return src('./src/sass/**/*.sass')
       .pipe(sass())
-      .pipe(gulp.dest('./src/css'))
+      .pipe(dest('./src/css'))
       .pipe(browserSync.stream())
 }
 
-function watch() {
+function server() {
   browserSync.init({
     server: {
       baseDir: "./src/"
     }
   })
-  gulp.watch('./src/sass/**/*.sass', style);
-  gulp.watch("./src/*.html").on('change', browserSync.reload);
+  watch("./src/sass/**/*.sass", style);
+  watch("./src/js/**/*.js", style);
+  watch("./src/*.html").on('change', browserSync.reload);
 }
 
 exports.style = style;
-exports.watch = watch;
+exports.server = server;
 exports.minify = minify;
