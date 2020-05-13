c4007982baf5b58741928562acbe129979c30e89
vsbdn <v.boldurean@gmail.com>
2020-05-13 16:34:45 +0100

    feat: Added listeners for closing modal on keypress and click out of the modal dialog.

--- src/js/main.js
+++ src/js/main.js
@@ -6,18 +6,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const modalBtn = document.querySelectorAll('[data-toggle=modal');
     const closeBtn = document.querySelector('.modal__close');
 
+
     //функция добавления класса
     const switchModal = () => {
         modal.classList.toggle('modal--visible');
     }
 
+
     //добавляем события:
 
-    //прослушка кнопок
+    //прослушка кнопок "открыть"
     modalBtn.forEach(element => {
        element.addEventListener('click', switchModal);
     });
-
+    //прослушка кнопки "закрыть"
     closeBtn.addEventListener('click', switchModal);
+
+    //закрытие окна по клику мимо
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.classList.remove('modal--visible');
+        }
+    }
+    //событие по кнопке ESC
+    window.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            modal.classList.remove('modal--visible');
+        }
+    })
 
 })
