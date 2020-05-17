903301b7a5593b085ca76a0abb36574902981026
vsbdn <v.boldurean@gmail.com>
2020-05-17 15:55:10 +0100

    feat: added second slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,3 @@
-// //Ждём пока загрузится DOM
-// document.addEventListener("DOMContentLoaded", function (event) {
-//
-//     //получаем кнопки
-//     const modal = document.querySelector('.modal');
-//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-//     const closeBtn = document.querySelector('.modal__close');
-//
-//
-//     //функция добавления класса
-//     const switchModal = () => {
-//         modal.classList.toggle('modal--visible');
-//     }
-//
-//     //добавляем события
-//
-//     //прослушка кнопок "открыть"
-//     modalBtn.forEach(element => {
-//        element.addEventListener('click', switchModal);
-//     });
-//
-//     //прослушка кнопки "закрыть"
-//     closeBtn.addEventListener('click', switchModal);
-//
-//     //закрытие окна по клику мимо
-//     window.onclick = function(event) {
-//         if (event.target === modal) {
-//             modal.classList.remove('modal--visible');
-//         }
-//     }
-//     //событие по кнопке ESC
-//     window.addEventListener('keydown', function (event) {
-//         if (event.key === 'Escape') {
-//             modal.classList.remove('modal--visible');
-//         }
-//     })
-// })
-
 $(document).ready(function () {
     const modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
@@ -135,6 +97,7 @@ $(document).ready(function () {
 
     });
 
+    //Контроллер вторым слайдером - первый
     swiperStepsTwo.controller.control = swiperStepsOne;
 
     const stepNext = $('.steps__swiper-button-next');
@@ -142,6 +105,8 @@ $(document).ready(function () {
     const stepBullets = $('.steps__swiper-pagination--bullets');
 
     stepNext.css('left', stepPrev.width() + 10 + stepBullets.width() + 35);
+
+    //Получаем кнопки + события по клику
 
     $(".button-0").click(function () {
         swiperStepsTwo.slideTo(0);
@@ -162,7 +127,7 @@ $(document).ready(function () {
         swiperStepsTwo.slideTo(5);
     });
 
-
+    //добавляем и убираем нужный класс чтобы покрасить активный слайд в белый.
     swiperStepsTwo.on('slideChange', function () {
         var activeSlide = ('.button-' + swiperStepsTwo.realIndex);
         var prevSlide = ('.button-' + swiperStepsTwo.previousIndex);
