b84eabd7c9308c8c3a4fe7fd80029581064d2a5e
vsbdn <v.boldurean@gmail.com>
2020-05-20 23:42:07 +0100

    Feat: clients section, refactored bg.

--- src/js/main.js
+++ src/js/main.js
@@ -185,4 +185,38 @@ $(document).ready(function () {
 
     });
 
+    //  yandex map api
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [47.244729, 39.723187],
+                zoom: 9
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+
+            // Создаём макет содержимого.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
+
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Собственный значок метки',
+                balloonContent: 'Это красивая метка'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'img/location.png',
+                // Размеры метки.
+                iconImageSize: [32, 32],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-5, -38]
+            });
+
+        myMap.geoObjects
+            .add(myPlacemark)
+    });
+
 });
